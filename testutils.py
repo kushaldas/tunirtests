8165f5209df85ac07b5ecf9650eccc762ea112f5
Kushal Das <kushaldas@gmail.com>
2015-03-18 13:12:24 +0530

    Initial code to have some utility functions.

---
+++ testutils.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def system(cmd):
+    """
+    Invoke a shell command.
+
+    :returns: A tuple of output, err message, and return code
+    """
+    ret = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    out, err = ret.communicate()
+    return out, err, ret.returncode
