cb5dd7558677350be7589ef1d5f8104b0c055c24
Kushal Das <kushaldas@gmail.com>
2015-03-19 17:30:40 +0530

    Updates the tests.

--- cloudtests.py
+++ cloudtests.py
@@ -7,16 +7,15 @@ class TestBase(unittest.TestCase):
     @unittest.expectedFailure
     def test_selinux(self):
         "Tests the SELinux"
-        out, err, eid = system('getenforce')
+        out, err, eid = system('sudo getenforce')
         out = out.strip()
         self.assertEquals(out, 'Enforcing')
 
     def test_logging(self):
         "Tests journald logging"
-        out, err, eid = system('journalctl -a --no-pager -r --since=$(date +%Y-%m-%d) -n1')
+        out, err, eid = system('sudo journalctl -a --no-pager -r --since=$(date +%Y-%m-%d) -n1')
         self.assertGreater(len(out.split()), 3, "journalctl output is missing.")
 
-    @unittest.expectedFailure
     def test_services(self):
         "No service should fail in the startup."
         out, err, eid = system('systemctl --all --failed')
