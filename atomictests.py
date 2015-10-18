3c455c5d45268f78ceb89589606bed172dcdacd2
Ratnadeep Debnath <rtnpro@gmail.com>
2015-10-18 22:22:40 +0530

    Add docs for atomic tests.

--- atomictests.py
+++ atomictests.py
@@ -42,7 +42,7 @@ class TestDockerStorageSetup(unittest.TestCase):
 @unittest.skipUnless(if_atomic(), "It's not an Atomic image")
 class TestDockerInstalled(unittest.TestCase):
 
-    def test_success(self):
+    def test_run(self):
         out, err, eid = system('rpm -q docker')
         out = out.decode('utf-8')
         self.assertTrue('docker' in out)
@@ -58,12 +58,18 @@ class TestAtomicUpgradeRun(unittest.TestCase):
         out, err, eid = system('sudo ostree admin status')
         out = out.decode('utf-8')
         self.assertTrue(out)
+
+        # We create a file /etc/file1 before running rpm-ostree upgrade.
+        # This file should persist, even in rolling back the upgrade.
+        # This we assert in
+        # TestAtomicRollbackPostReboot.test_atomic_rollback_post_reboot
         out, err, eid = system(
             'sudo cat /ostree/repo/refs/heads/ostree/0/1/0 > /etc/file1')
         err = err.decode('utf-8')
         self.assertFalse(err)
         out, err, eid = system('sudo rpm-ostree upgrade')
         err = err.decode('utf-8')
+        # Assert successful run
         self.assertFalse(err)
 
 
@@ -81,6 +87,9 @@ class TestAtomicUpgradePostReboot(unittest.TestCase):
 class TestAtomicRollbackRun(unittest.TestCase):
 
     def test_atomic_rollback_run(self):
+        # We make changes to the system by creating /etc/file2 before
+        # running rollback. Once rollback is run, /etc/file2 will be
+        # removed. We assert that in the following test case.
         out, err, eid = system(
             'sudo cat /ostree/repo/refs/heads/ostree/1/1/0 > /etc/file2')
         err = err.decode('utf-8')
@@ -99,14 +108,17 @@ class TestAtomicRollbackPostReboot(unittest.TestCase):
         out = out.decode('utf-8')
         self.assertTrue(out)
 
+        # Assert that file1 is present
         out, err, eid = system('sudo cat /etc/file1')
         out = out.decode('utf-8')
         self.assertTrue(out)
 
+        # Assert that file2 is not present
         out, err, eid = system('sudo cat /etc/file2')
         err = err.decode('utf-8')
         self.assertTrue(err)
 
+        # Assert that running busybox docker image works after rollback
         out, err, eid = system(
             'docker run --rm busybox true && echo "PASS"')
         out = out.decode('utf-8')
