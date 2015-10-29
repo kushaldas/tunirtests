7554ed22f245e9a884ed213b82fb6c2cbb2c284f
Kushal Das <kushaldas@gmail.com>
2015-10-29 19:55:16 +0530

    updated publish.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/sh
 
+rm -rf tunirtests
 mkdir -p tunirtests
-cp *.py tunirtests/
+cp -f *.py tunirtests/
 cp LICENSE tunirtests
 tar -czvf /tmp/tunirtests.tar.gz tunirtests
-scp /tmp/tunirtests.tar.gz kushal@fedorapeople.org:./public_html/
+rsync -avz --progress /tmp/tunirtests.tar.gz kushal@fedorapeople.org:~/public_html/
